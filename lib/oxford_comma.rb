fbf85d73c80d7d8b2a452eae3bd95ff6fb3f1212
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-07 21:35:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,16 @@
 require "pry"
 def oxford_comma(array)
   if array.count <=1
-  array.join(" ") 
+  array.join(" ")
   elsif array.count <=2
     removed_item = array.pop
     array<< "and #{removed_item}"
     array.join(" ")
-  elsif array.count >=3 
+  elsif array.count >=3
   removed_item= array.pop
   array<<"and #{removed_item}"
   array.join(", ")
 
-  else  
+  else
   end
 end
-
